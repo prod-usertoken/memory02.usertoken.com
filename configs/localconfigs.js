07867b6974bd961d3c024fd83af0760d549aee3c
Alex Davis <alex2006hw@gmail.com>
2017-12-26 20:10:21 -0800

    201712.26.1514347818

--- configs/localconfigs.js
+++ configs/localconfigs.js
@@ -4,13 +4,13 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 /////
-var HOSTNAME = exports.HOSTNAME = "memory02.usertoken.com";
+var HOSTNAME = exports.HOSTNAME = "memory02.alex2006hw.com";
 var DATA_FILE = exports.DATA_FILE = "data/data-" + HOSTNAME + "-server";
 var DEVICE_ID = exports.DEVICE_ID = false;
-var CLOUD_MEMORIES = exports.CLOUD_MEMORIES = ["https://memory02-memory02-ut.193b.starter-ca-central-1.openshiftapps.com/gun", "https://memtwo-memory02.193b.starter-ca-central-1.openshiftapps.com/gun"];
-var MY_MEMORY = exports.MY_MEMORY = "https://memory02.usertoken.com";
+var CLOUD_MEMORIES = exports.CLOUD_MEMORIES = ["https://tropospheric.mybluemix.net/gun", "https://tropospheric-tropospheric.193b.starter-ca-central-1.openshiftapps.com/gun"];
+var MY_MEMORY = exports.MY_MEMORY = "https://memory02.alex2006hw.com";
 var PEER_MEMORIES = exports.PEER_MEMORIES = ["https://memory02.alex2006hw.com/gun", "https://memory02.pointlook.com/gun", "https://memory02.usertoken.com/gun"];
-var CHILD_MEMORIES = exports.CHILD_MEMORIES = ["https://tropospheric.mybluemix.net/gun", "https://tropospheric-tropospheric.193b.starter-ca-central-1.openshiftapps.com/gun", "https://memory02-memory02-pl.193b.starter-ca-central-1.openshiftapps.com/gun", "https://memory02-memory02-ut.193b.starter-ca-central-1.openshiftapps.com/gun", "https://memory02-memory02-alex.193b.starter-ca-central-1.openshiftapps.com/gun", "https://memtwo-memory02.193b.starter-ca-central-1.openshiftapps.com/gun"];
+var CHILD_MEMORIES = exports.CHILD_MEMORIES = ["https://tropospheric.mybluemix.net/gun", "https://tropospheric-tropospheric.193b.starter-ca-central-1.openshiftapps.com/gun", "https://memory02-memory02-pl.193b.starter-ca-central-1.openshiftapps.com/gun", "https://m2-admin-ut-m2-admin-ut.193b.starter-ca-central-1.openshiftapps.com/gun", "https://m2-alex2006hw.193b.starter-ca-central-1.openshiftapps.com/gun", "https://m2-memtwo.193b.starter-ca-central-1.openshiftapps.com/gun"];
 /////
 var GRAPHQL = exports.GRAPHQL = "https://" + HOSTNAME + "/api/graphql";
 var DBFILE = exports.DBFILE = DATA_FILE;
