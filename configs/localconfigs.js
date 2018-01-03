d2a6f4fea5fe267d93cf6436141a135f8e21c569
Alex Davis <alex2006hw@gmail.com>
2018-01-03 14:07:08 -0800

    201801.03.1515017219

--- configs/localconfigs.js
+++ configs/localconfigs.js
@@ -4,19 +4,37 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 /////
-var HOSTNAME = exports.HOSTNAME = "memory02.usertoken.com";
-var DATA_FILE = exports.DATA_FILE = "data/data-" + HOSTNAME + "-client";
+var HOSTNAME = exports.HOSTNAME = "memory02.pointlook.com";
+var DATA_FILE = exports.DATA_FILE = "data/data-" + HOSTNAME;
 var DEVICE_ID = exports.DEVICE_ID = false;
-
-var MY_MEMORY = exports.MY_MEMORY = CLOUD_MEMORIES;
-var CLOUD_MEMORIES = exports.CLOUD_MEMORIES = "https://m2-ut-admin.193b.starter-ca-central-1.openshiftapps.com/gun";
-var DEV_CLOUD_MEMORIES = exports.DEV_CLOUD_MEMORIES = ["https://dev-ut-redhat.193b.starter-ca-central-1.openshiftapps.com/gun", "https://dev-ut-memtwo.193b.starter-ca-central-1.openshiftapps.com/gun"];
-var ROOT_MEMORIES = exports.ROOT_MEMORIES = ["https://tropospheric.mybluemix.net/gun", "https://tropospheric-tropospheric.193b.starter-ca-central-1.openshiftapps.com/gun"];
+/////
+var GRAPHQL = exports.GRAPHQL = "https://" + HOSTNAME + "/api/graphql";
+var DBFILE = exports.DBFILE = DATA_FILE;
+var DEBUG = exports.DEBUG = false;
+var DEBUG_LOG_URL = exports.DEBUG_LOG_URL = "https://logentries.com/app/a092e388";
+/////
+//export const MY_MEMORY =
+//  !window ||
+//  !window.location ||
+//  !window.location.protocol ||
+//  !window.location.host
+//    ? "https://m2-alex2006hw-gmail.7e14.starter-us-west-2.openshiftapps.com/gun"
+//    : window.location.protocol + "//" + window.location.host + "/gun";
+//
+//export const CLOUD_MEMORIES = MY_MEMORY;
+//export const DEV_CLOUD_MEMORIES = [
+//  "https://dev-ut-redhat.193b.starter-ca-central-1.openshiftapps.com/gun",
+//  "https://dev-ut-memtwo.193b.starter-ca-central-1.openshiftapps.com/gun"
+//];
+//export const ROOT_MEMORIES = [
+//  "https://tropospheric.mybluemix.net/gun",
+//  "https://tropospheric-tropospheric.193b.starter-ca-central-1.openshiftapps.com/gun"
+//];
 //export const MY_MEMORY = "https://memory02.alex2006hw.com/gun";
 //export const ROOT_MEMORIES = [
 //  "https://memory02.alex2006hw.com/gun",
 //  "https://memory02.pointlook.com/gun",
-//  "https://memory02.alex2006hw.com/gun"
+//  "https://memory02.usertoken.com/gun"
 //];
 //export const PEER_MEMORIES = [
 //  "https://tropospheric.mybluemix.net/gun",
@@ -26,8 +44,3 @@ var ROOT_MEMORIES = exports.ROOT_MEMORIES = ["https://tropospheric.mybluemix.net
 //  "https://m2-alex2006hw.193b.starter-ca-central-1.openshiftapps.com/gun",
 //  "https://m2-memtwo.193b.starter-ca-central-1.openshiftapps.com/gun"
 //];
-/////
-var GRAPHQL = exports.GRAPHQL = "https://" + HOSTNAME + "/api/graphql";
-var DBFILE = exports.DBFILE = DATA_FILE;
-var DEBUG = exports.DEBUG = false;
-var DEBUG_LOG_URL = exports.DEBUG_LOG_URL = "https://logentries.com/app/a092e388";
